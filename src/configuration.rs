5aa875732203a82e05cf88e81140cb59c2e70b19
Kishore Karthick <kishorekarthick183@gmail.com>
2026-08-16 02:06:13 +0530

    Add PostgreSQL configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,6 +4,25 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct Settings {
     pub application_port: u16,
+    pub database: DatabaseSettings,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("failed to read configuration");
+    let connect_pool = PgPoolOptions::new()
+        .connect_lazy(&configuration.database.connection_string())
+        .expect("failed to connect to connection pool");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     // TODO: try with single underscore
     let listener = TcpListener::bind(address)?;
