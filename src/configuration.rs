c826bc24332ec348af86d424345b3129af619bf1
Kishore Karthick <kishorekarthick183@gmail.com>
2026-08-16 01:44:49 +0530

    Test application configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -12,3 +12,14 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .build()?;
     settings.try_deserialize()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn configuration_can_be_loaded() {
+        let configuration = get_configuration().expect("Failed to read configuration");
+        assert_eq!(configuration.application_port, 8000);
+    }
+}
